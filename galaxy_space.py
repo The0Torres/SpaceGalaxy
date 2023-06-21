00da30ee753f79b3c5153e0a5059602c4a5ce8fd
theo <theotocar@gmail.com>
2023-06-21 10:27:18 -0300

    Capitane del espacio

--- galaxy_space.py
+++ galaxy_space.py
@@ -121,37 +121,48 @@ def usuario_puntuacion(score):
 
 def puntuaciones():
     pygame.display.set_caption("Puntuaciones")
-    
+
+    conexion = sqlite3.connect('usuarios.db')
+    cursor = conexion.cursor()
+
+    datos_disponibles = False
+
+    try:
+        cursor.execute("SELECT nombre, puntacion FROM usuarios ORDER BY puntacion DESC LIMIT 10")
+        resultados = cursor.fetchall()
+        if resultados:
+            datos_disponibles = True
+    except sqlite3.OperationalError:
+        # La base de datos no existe o la tabla no existe
+        datos_disponibles = False
+
     while True:
         pantalla.blit(fondo_menu, fondo_menu.get_rect())
 
         poscion_mouse = pygame.mouse.get_pos()
 
-        conexion = sqlite3.connect('usuarios.db')
-        cursor = conexion.cursor()
-
-        cursor.execute("SELECT nombre, puntacion FROM usuarios ORDER BY puntacion DESC LIMIT 10")
-        resultados = cursor.fetchall()
-
-        boton_menu = Boton(boton , 600, 900, "Menu")
-
+        boton_menu = Boton(boton, 600, 900, "Menu")
         boton_menu.cambiarColor(poscion_mouse)
         boton_menu.actualizar()
 
         font_puntuaciones = pygame.font.SysFont("cambria", 100)
-
-        mejores_score = font_puntuaciones.render("Mejores puntuaciones", True , colores.YELLOW1)
+        mejores_score = font_puntuaciones.render("Mejores puntuaciones", True, colores.YELLOW1)
         pantalla.blit(mejores_score, (190, 100))
 
-        font = pygame.font.SysFont("cambria", 50)
-        y = 300
-        posicion_juego = 1
+        if datos_disponibles:
+            font = pygame.font.SysFont("cambria", 50)
+            y = 300
+            posicion_juego = 1
 
-        for nombre, puntacion in resultados:
-            texto = font.render(f"{posicion_juego}. {nombre} - {puntacion}", True, colores.YELLOW1)
-            pantalla.blit(texto, (440, y))
-            y += 50
-            posicion_juego += 1
+            for nombre, puntacion in resultados:
+                texto = font.render(f"{posicion_juego}. {nombre} - {puntacion}", True, colores.YELLOW1)
+                pantalla.blit(texto, (440, y))
+                y += 50
+                posicion_juego += 1
+        else:
+            font_no_datos = pygame.font.SysFont("cambria", 50)
+            sin_data = font_no_datos.render("Datos aún no registrados", True, colores.YELLOW1)
+            pantalla.blit(sin_data, (350, 300))
 
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
@@ -162,6 +173,7 @@ def puntuaciones():
                     menu_galaxy()
 
         pygame.display.update()
+
 
 def juego():
     pygame.mixer.init()
